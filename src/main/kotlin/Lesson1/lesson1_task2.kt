2241e64e9bf08e787b6a01ce77d4e47c822c5c37
utka47 <utka0047@gmail.com>
2024-11-23 23:35:15 +0500

    KS-1-2 task accomplished

---
+++ src/main/kotlin/Lesson1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson1
+
+fun main() {
+
+    var numberOfOrders: Byte = 75
+    val thankYouText: String = "Благодарим за покупку"
+    println("Заказ номер $numberOfOrders. $thankYouText")
+
+    var numberOfStaff: Short = 2000
+//    println(numberOfStaff)
+    numberOfStaff = 1999
+    println("Количество сотрудников - $numberOfStaff человек")
+
+}
