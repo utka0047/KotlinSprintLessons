50d557c7dfefcd51156f02dbf89bab7abe095fb6
utka47 <utka0047@gmail.com>
2024-11-25 12:11:51 +0500

    KS-2-1 redesigned task

--- src/main/kotlin/Lesson2/lesson1_task1.kt
+++ src/main/kotlin/Lesson2/lesson1_task1.kt
@@ -2,9 +2,9 @@ package Lesson2
 
 fun main() {
 
-    var students = 4f
-    var scores = 3 + 4 + 3 + 4
-    var average = scores / students
+    val students = 4
+    val scores = 3 + 4 + 3 + 4
+    val average = (scores).toFloat() / students
     val formattedAverage = "%.2f".format(average)
 
     println(formattedAverage)
