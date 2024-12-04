6d8e1f901914a8b367ca8ca4b58b91eef3940941
utka47 <utka0047@gmail.com>
2024-12-04 20:46:39 +0500

    KS-4-2 remove magic number

--- src/main/kotlin/Lesson5/lesson5_task1.kt
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -3,18 +3,21 @@ package Lesson5
 
 fun main() {
 
-    val task = "2+2"
-    val taskResponse = 2+2
-    println("""
+    val num1 = 2
+    val num2 = 2
+    val task = "$num1 + $num2"
+    val taskResponse = num1 + num2
+    println(
+        """
         |Решите задачу, чтобы войти
-        |$task""".trimMargin())
+        |$task""".trimMargin()
+    )
 
     val captcha = readln().toInt()
 
     if (captcha == taskResponse) {
         println("Добро пожаловать!")
-    }
-    else {
+    } else {
         println("Доступ запрещен.")
     }
 
