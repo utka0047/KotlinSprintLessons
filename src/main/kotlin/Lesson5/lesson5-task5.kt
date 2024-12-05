ca3e5c9331104e597c77ceb2995e56dd0c40563d
utka47 <utka0047@gmail.com>
2024-12-05 21:03:45 +0500

    KS-5-7 remade random

--- src/main/kotlin/Lesson5/lesson5-task5.kt
+++ src/main/kotlin/Lesson5/lesson5-task5.kt
@@ -1,23 +1,13 @@
-package Lesson5
-
-import kotlin.random.Random
-
-
-
 fun main() {
-
-    val secretNumbers = List(3) {Random.nextInt(0,43)}
-
+    val secretNumbers = List(3) {(0..43).random()}
     println("Угадайте три числа от 0 до 42")
     val userNumbers = List(3) { readln().toInt()}
     val matches = secretNumbers.intersect(userNumbers.toSet()).size
-
     when (matches) {
         3 -> println("Вы угадали все числа и выйграли джекпот!")
         2 -> println("Вы угадали два числа и выйграли крупный приз!")
         1 -> println("Вы угадали одно число и выйграли утешительный приз.")
         else -> println("Вы не угадали ни одного числа и ничего не выйграли.")
     }
-
     println("Выигрышные числа: $secretNumbers")
 }
